3975530f167594e0848d1dadfebb4b8ca9b07b2c
Dániel Nemeskéri <ap23bnd@student.elte.hu>
2025-04-01 22:08:47 +0200

    Add files via upload

---
+++ masodik bead/build/CMakeFiles/TestMatrix.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestMatrix.dir/test_matrix.cpp.o"
+  "CMakeFiles/TestMatrix.dir/test_matrix.cpp.o.d"
+  "TestMatrix"
+  "TestMatrix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestMatrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
